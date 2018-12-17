68dee1d28eaf2aa5546c8fab9c2babc0a271d757
adhamehab <adhaamehab7@gmail.com>
2018-12-17 20:25:19 +0200

    Add textcorrection interface

--- textblob_ar/__init__.py
+++ textblob_ar/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 from textblob_ar.blob import TextBlob
 from textblob_ar.similarity import TextSimilarity
+from textblob_ar.correction import TextCorrection
 
 __version__ = '0.0.2'
 __author__ = 'Adham Ehab'
